594f8d2e6604bdab0ae1232c500498e44e9321ef
dndkwk <dndkwk@naver.com>
2021-11-03 22:44:22 +0900

    1:N 구현

--- 7/models/comment.js
+++ 7/models/comment.js
@@ -22,5 +22,7 @@ module.exports = class User extends Sequelize.Model {
       }
     );
   }
-  static associate(db) {}
+  static associate(db) {
+    db.User.belongsTo(db.User, { foreignKey: "commenter", targetKey: "id" });
+  }
 };

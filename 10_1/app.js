a98da6dbed084d1e910287d539ba01e8680988e3
dndkwk <dndkwk@naver.com>
2021-11-22 21:40:48 +0900

    no message

--- 10_1/app.js
+++ 10_1/app.js
@@ -1,40 +1,21 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
-const passport = require("passport");
 const morgan = require("morgan");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const authRouter = require("./routes/auth");
 const indexRouter = require("./routes");
-const { sequelize } = require("./models");
-const passportConfig = require("./passport");
-const v1 = require("./routes/v1");
 
 const app = express();
-passportConfig();
-app.set("port", process.env.PORT || 8002);
+app.set("port", process.env.PORT || 4000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
@@ -47,18 +28,13 @@ app.use(
     },
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
-app.use("/v1", v1);
-app.use("/auth", authRouter);
-app.use("/", indexRouter);
 
+app.use("/", indexRouter);
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
   next(error);
 });
-
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};

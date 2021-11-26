ad09851f1eea57d126a3eac0d0acdac5cee82e8e
dndkwk <dndkwk@naver.com>
2021-11-27 00:24:31 +0900

    no message

--- 12/app.js
+++ 12/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
+const ColorHash = require("color-hash");
 
 dotenv.config();
 const webSocket = require("./socket");
@@ -31,6 +32,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
+
 app.use("/", indexRouter);
 
 app.use((req, res, next) => {
@@ -50,4 +59,4 @@ const server = app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
 
-webSocket(server);
+webSocket(server, app);
